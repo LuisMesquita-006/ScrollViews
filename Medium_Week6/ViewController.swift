1f8e9b8177130ac14e65db9129913f6b27a10edf
LuisMesquita-006 <lcmproducoespt@gmail.com>
2020-04-04 23:32:58 +0100

    Initial Commit

---
+++ Medium_Week6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medium_Week6
+//
+//  Created by Luis Mesquita on 04/04/2020.
+//  Copyright © 2020 Luis Mesquita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
